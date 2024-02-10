ef2d9c57d26c4e3a7b3daf6a7328a0f3dbd464f0
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-02-10 15:19:44 +0700

    Initial Commit

---
+++ Udacity_PitchPerfect/Udacity_PitchPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udacity_PitchPerfectApp.swift
+//  Udacity_PitchPerfect
+//
+//  Created by Work on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Udacity_PitchPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
